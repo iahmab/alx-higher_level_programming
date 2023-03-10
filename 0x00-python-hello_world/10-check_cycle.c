f3dd47e9d7bee14521be45bfcbc09d559c26eef0
iahmab <idrisabraham92@gmail.com>
2023-03-10 00:58:39 -0800

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,38 @@
+/*
+ * Files: 10-check_cycle.c
+ * Auth: Abraham Idris
+ */
+
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * #list: Asingly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ * 		If there is a cycle - 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	tutle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
+}
+
+
